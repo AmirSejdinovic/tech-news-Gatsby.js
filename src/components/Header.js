5692184a5c3744d65ca49c522f5ec87ed1b3dc3d
Amir Sejdinovic <amir24V@yahoo.com>
2021-06-02 14:14:53 +0200

    Adding styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Link } from 'gatsby'
+import * as headerStyles from './Header.module.scss';
 
 export default function Header(){
   return(
     <div>
-      <h1>I am header</h1>
+      <h1 className={headerStyles.headerTitle}>I am header</h1>
+      <h3 className={headerStyles.headerSub}>Header subtite</h3>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/blogs">Blogs</Link>
