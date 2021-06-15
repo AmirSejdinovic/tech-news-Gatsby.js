866f8ba8d3231be738c32de4566355876ee17bf3
Amir Sejdinovic <amir24V@yahoo.com>
2021-06-15 10:22:33 +0200

    Creating md files and display it info on gatsby page

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { graphql } from 'gatsby'
 
-export default function Blogs(){
+export default function Blogs({data}){
+
+  console.log(data.allFile.nodes)
   return(
     <Layout>
-      <h1>I am blogs page</h1>
+      <ul>
+      {
+       data.allFile.nodes.map(node =>
+         <li key={node.relativePath}>
+           <p>{node.relativePath}</p>
+           <p>{node.ext}</p>
+           <p>{node.birthTime}</p>
+           <p>{node.prettySize}</p>
+         </li>
+        )
+      }
+      </ul>
     </Layout>
   )
 }
+
+//AllFile
+export const query = graphql`
+  query{
+    allFile {
+      nodes {
+        id
+        ext
+        relativePath
+        birthTime(fromNow: true)
+        prettySize
+      }
+    }
+  }
+`
